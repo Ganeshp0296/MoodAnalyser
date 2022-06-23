a622e67521ac2c2da22a86d0923277352ad28ddb
Ganeshp0296 <Ganeshp457@gmail.com>
2022-06-23 16:46:27 +0530

    adding tc1

---
+++ src/com/bridgelabz/MoodAnalyserTest.java
@@ -0,0 +1,26 @@
+package com.bridgelabz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+	// Test case to analyze sad mood of the user
+    @Test
+    public void givenMessageWhenProperShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String actualResult = MoodAnalyser.analyseMood("i am in sad mood");
+        Assert.assertEquals("Sad", actualResult);
+    }
+    @Test
+    public void givenMessageContainsAnyInitShouldReturnHappy() {
+        MoodAnalyser moodanalyser = new MoodAnalyser();
+        String actualResult = MoodAnalyser.analyseMood("I am in Any Mood");
+        Assert.assertEquals("Happy", actualResult);
+    }
+}
+
+
+
+
