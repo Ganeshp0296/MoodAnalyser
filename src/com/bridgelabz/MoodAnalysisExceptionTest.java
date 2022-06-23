d4244d3605cb381ec31e9aaa5eb147fd7bdfb15f
Ganeshp0296 <Ganeshp457@gmail.com>
2022-06-23 17:17:01 +0530

    adding uc3_custom_exception

---
+++ src/com/bridgelabz/MoodAnalysisExceptionTest.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import org.junit.Assert;
+
+public class MoodAnalysisExceptionTest {
+	// Test case to analyze sad mood of the user
+	@Test
+    public void givenMessage_WhenProper_ShouldReturnSad() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+        //moodAnalyser.setMessage("I am in Sad Mood");
+        String actualResult = MoodAnalyser.analyseMood();
+        Assert.assertEquals("Sad", actualResult);
+    }
+
+    @Test
+    public void givenMessage_WhenProper_ShouldReturnHappy() throws MoodAnalysisException {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("I am in happy Mood");
+        String actualResult = MoodAnalyser.analyseMood();
+        Assert.assertEquals("Happy", actualResult);
+    }
+
+    @Test
+    public void givenMessage_Null_ShouldReturnMessage() throws MoodAnalysisException {
+        MoodAnalyser moodanalyser = new MoodAnalyser(null);
+        try {
+            String actualResult = MoodAnalyser.analyseMood();
+            Assert.assertEquals("Entered Invalid Mood ", actualResult);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+
